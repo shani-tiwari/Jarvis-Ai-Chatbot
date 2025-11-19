a702e4bf1e9e78a7abeed8e1eb0b37f7046a8c5c
shani-tiwari <shanitiwari2021@gmail.com>
2025-11-19 17:16:16 +0530

    socket error

--- Backend/src/socket/socket.server.js
+++ Backend/src/socket/socket.server.js
@@ -11,7 +11,7 @@ function initSocketServer(httpServer) {
 
     const io = new Server(httpServer, {
         cors: { 
-            origin: "http://localhost:5173",
+            origin: "https://jarvis-ai-chatbot.onrender.com",
             credentials: true
          }
     });
